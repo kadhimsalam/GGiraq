9ef93e7a33e9b5eaf75b83a8dff85c79a364154f
kadhimsalam <kadhimsalam33@gmail.com>
2026-07-11 08:51:00 +0300

    Initialize Supabase client in database.js

--- js/database.js
+++ js/database.js
@@ -1,1 +1,8 @@
-console.log("database");
+const SUPABASE_URL = "https://bsfisveqboywdbvrcztj.supabase.co/rest/v1/";
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzZmlzdmVxYm95d2RidnJjenRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NDEyMDYsImV4cCI6MjA5OTMxNzIwNn0.1xQFEKdNpaz5osOHE1ri58RBnWLj1NB0F7nwEnoDI90";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
